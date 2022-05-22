8e64eaae09bf4c3e4dd36687bd2e2f2f325c9106
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-05-22 21:57:17 +0100

    0-0

--- dialogue/state_logic.py
+++ dialogue/state_logic.py
@@ -3,14 +3,16 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from dialogue.states import DialogueState
+    from dialogue.manager import DialogueManager
 
-def confirm_handler(self: "DialogueState"):
+
+def confirm_handler(self: "DialogueState", confirm_message="Please confirm the following"):
     """ Logic to handle confirmation"""
     missing_details = []
     for e in self.entity_mask:
         if e not in self.state_entities:
             missing_details.append(e)
-    
+
     if missing_details:
         return_string = "You are missing the following:\n"
         for v in missing_details:
@@ -19,7 +21,7 @@ def confirm_handler(self: "DialogueState"):
         self.turn = "unknown"
 
     else:
-        return_string = "Please confirm the following:\n"
+        return_string = f"{confirm_message}:\n"
         for k, v in self.state_entities.items():
             return_string += f'  - For {k.title()} you entered: {v}\n'
         return_string += '\nYou can say "Yes" or "No" to confirm.'
@@ -36,48 +38,81 @@ def init_logic(self: "DialogueState"):
 
 def check_availability_logic(self: "DialogueState"):
     """Logic for the check availability state"""
-    if self.turn == "confirm":
-        return confirm_handler(self)
-    else:
-        return f"{self.name}, {self.turn}"
+    return 'OPTIONS'
 
 
 def add_to_basket_logic(self: "DialogueState"):
     """Logic for the add to basket state"""
-    if self.turn == "confirm":
-        return confirm_handler(self)
-    else:
-        return f"{self.name}, {self.turn}"
+    search_query = self.state_entities['PRODUCT']
+    return 'OPTIONS'
 
 
 def remove_from_basket_logic(self: "DialogueState"):
     """Logic for the remove from basket state"""
+
+    def confirmed_callback(manager: "DialogueManager"):
+        search_query = self.state_entities['PRODUCT']
+        # TODO: Actually remove the correct item or decrement, make sure it exists
+        manager.finalised_values['items'].remove(0)
+
     if self.turn == "confirm":
-        return confirm_handler(self)
+        
+
+        return confirm_handler(self, "Please confirm removing the following from the basket")
+    elif self.turn == "confirmed":
+        return confirmed_callback
     else:
         return f"{self.name}, {self.turn}"
 
 
 def address_details_logic(self: "DialogueState"):
     """Logic for the address details state"""
+
+    def confirmed_callback(manager: "DialogueManager"):
+        manager.finalised_values['address'] = {
+            'STREET': self.state_entities['STREET'],
+            'CITY': self.state_entities['CITY'],
+            'STREET': self.state_entities['POSTCODE'],
+        }
+
     if self.turn == "confirm":
         return confirm_handler(self)
+    elif self.turn == "confirmed":
+        return confirmed_callback
     else:
         return f"{self.name}, {self.turn}"
 
 
 def timeslot_details_logic(self: "DialogueState"):
     """Logic for the timeslot request state"""
+
+    def confirmed_callback(manager: "DialogueManager"):
+        manager.finalised_values['timeslot'] = self.state_entities['TIME']
+
     if self.turn == "confirm":
         return confirm_handler(self)
+    elif self.turn == "confirmed":
+        return confirmed_callback
     else:
         return f"{self.name}, {self.turn}"
-    
+
 
 def payment_details_logic(self: "DialogueState"):
     """Logic for the payment details state"""
+
+    def confirmed_callback(manager: "DialogueManager"):
+        manager.finalised_values['payment'] = {
+            'NAME': self.state_entities['NAME'],
+            # TODO: Assumes they exist
+            'CARD_NUMBER': self.state_entities['CARD_NUMBER'],
+            'CARD_CVC': self.state_entities['CARD_CVC'],
+            'CARD_EXPIRY': self.state_entities['CARD_EXPIRY'],
+        }
+
     if self.turn == "confirm":
         return confirm_handler(self)
+    elif self.turn == "confirmed":
+        return confirmed_callback
     else:
         return f"{self.name}, {self.turn}"
 
