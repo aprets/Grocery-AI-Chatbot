810a9a8ca4581039678ab2395336e46498f23dc7
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-05-24 20:34:06 +0100

    update models

--- make_models.sh
+++ make_models.sh
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 python -m spacy download en_core_web_md
 python intent/make_model.py
 python intent/__init__.py
-python -m spacy train ner/model.cfg --training.max_epochs 15 --nlp.batch_size 64 --training.optimizer.@optimizers RAdam.v1 --output ner/out
+python -m spacy train ner/model.cfg --training.max_epochs 15 --output ner/out
 rm -rf ner_model
 mv ner/out/model-best ner_model
 deactivate
